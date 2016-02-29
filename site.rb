0e4c9e0e2fa6bd107342e627fa947f089b3820e6
Bishop Clark <bclark@cdn-dev-lab5.tpn.thinkingphones.net>
2016-02-29 15:56:03 -0500

    sanitize data

--- site.rb
+++ site.rb
@@ -1,4 +1,10 @@
-PROD_ACCOUNTNUM = "591511214639"
-S3_BUCKET       = 's3://straycat.dhs.org/os-images' # Used by awscli
-PROD_BUCKET     = "straycat.dhs.org/amis"           # Used by ec2-api-tools
+#
+#	D O   N O T   C O M M I T   R E A L   D A T A   I N T O   G I T H U B ! !
+#
+
+#PROD_ACCOUNTNUM = "591511214639"
+#S3_BUCKET       = 's3://straycat.dhs.org/os-images' # Used by awscli
+#PROD_BUCKET     = "straycat.dhs.org/amis"           # Used by ec2-api-tools
 AWS_PROFILE     = 'default'                         # profile used by awscli
+
+load ./real_site.sb
